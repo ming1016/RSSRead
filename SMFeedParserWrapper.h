0fc72a77d93924cfcef0bb5cd1190f38cb29a5e5
zaczh <jhzhang@mail.ru>
2014-05-17 01:16:13 +0800

    大量修改，增加界面流畅度，优化部分逻辑

---
+++ SMFeedParserWrapper.h
@@ -0,0 +1,13 @@
+//
+//  SMFeedParserWrapper.h
+//  RSSRead
+//
+//  Created by John Zhang on 5/15/14.
+//  Copyright (c) 2014 starming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMFeedParserWrapper : NSObject
+
+@end
---
+++ SMFeedParserWrapper.m
@@ -0,0 +1,13 @@
+//
+//  SMFeedParserWrapper.m
+//  RSSRead
+//
+//  Created by John Zhang on 5/15/14.
+//  Copyright (c) 2014 starming. All rights reserved.
+//
+
+#import "SMFeedParserWrapper.h"
+
+@implementation SMFeedParserWrapper
+
+@end
