b7ade267bf8682d151f5c03eea31e1da4ab4b5e8
ming <ming1016@gmail.com>
2015-06-22 11:17:49 +0800

    add singleton marcro

---
+++ RSSRead/SynthesizeSingletonForArc.h
@@ -0,0 +1,13 @@
+//
+//  SynthesizeSingletonForArc.h
+//  RSSRead
+//
+//  Created by ming on 15/6/22.
+//  Copyright (c) 2015年 starming. All rights reserved.
+//
+
+#ifndef RSSRead_SynthesizeSingletonForArc_h
+#define RSSRead_SynthesizeSingletonForArc_h
+
+
+#endif
